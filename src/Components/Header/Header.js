87cd167b476122b64c98ae0c5394a7abcea04983
Aashish Singhal <aashish451singhal@gmail.com>
2021-05-19 15:24:23 +0530

    added header and started work on it.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,41 @@
 import React from "react";
 import "./Header.css";
 import Logo from "../../Assets/tmm__logo.png";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import ForumIcon from "@material-ui/icons/Forum";
+import { Avatar } from "@material-ui/core";
+import Bell from "../../Assets/Bell";
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="header__logo">
-        <img src={Logo} alt="logo"  />
+      <div className="header__container">
+        <div className="header__logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        {/* <div className="header__vl"></div> */}
       </div>
-      <div className="header__search">
-          
+      <div className="header__container">
+        <div className="header__search">
+          <SearchIcon />
+          <input
+            type="text"
+            name="search"
+            placeholder="Search for student/tutor/project"
+          />
+        </div>
       </div>
-      <div className="header__nav-items"></div>
+      <div className="header__container">
+        <div className="header__nav-items">
+          <NotificationsActiveIcon className="nav-item noti__svg" />
+          {/* <Bell className="nav-item bell__svg" /> */}
+          <ForumIcon className="nav-item chat__svg" />
+          <div className="header__nav-items__avatar nav-item">
+            <Avatar className="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
