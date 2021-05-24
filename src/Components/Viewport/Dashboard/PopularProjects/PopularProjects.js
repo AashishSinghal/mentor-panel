38130e462eaf08e04e2d6704c6bafc7cebe930ed
Akash Agarwal <agarwalakash551999@gmail.com>
2021-05-25 00:47:28 +0530

    popular project

--- src/Components/Viewport/Dashboard/PopularProjects/PopularProjects.js
+++ src/Components/Viewport/Dashboard/PopularProjects/PopularProjects.js
@@ -31,8 +31,8 @@ const PopularProjects = () => {
         <div className="popularProjects">
             <div className="popularProjects_card1">
                 {projectTypeData.map((type)=> (
-                    <div key={type.name} className="popularProject_card1_type">
-                        <div className={`popularProject_card1_type_letter ${type.name}`}>
+                    <div key={type.name} className={`popularProject_card1_type popularProjects_card1_${type.caps}`}>
+                        <div className={`popularProject_card1_type_letter ${type.caps}`}>
                             {type.caps}
                         </div>
                         <div className="popularProject_card1_type_name">
@@ -42,6 +42,24 @@ const PopularProjects = () => {
                         <button className="popularProjects_card1_button">view all</button>
                     </div>
                 ))}
+            </div>
+            <div className="popularProject_card2">
+                <div className="popularProject_monthlyProgress">
+                    <p className="popularProject_monthlyProgress_head">Monthly Projects</p>
+                    <p className="popularProject_monthlyProgress_num">14+ Projects</p>      
+                </div>
+                <div className="popularProject_couter_cards">
+                    <div className="popularProject_couter_cards1">
+                        <p className="popularProject_couter_cards_total_text">Total<br/>Students</p>
+                        <p className="popularProject_couter_cards_total_num">1400+</p>
+                        <button className="popularProject_couter_cards_view_btn1">view</button>
+                    </div>
+                    <div className="popularProject_couter_cards2">
+                        <p className="popularProject_couter_cards_total_text">Total<br/>Students</p>
+                        <p className="popularProject_couter_cards_total_num">24+</p>
+                        <button className="popularProject_couter_cards_view_btn2">view</button>
+                    </div>
+                </div>
             </div>
             <div className="popularProjects_card1">
                 {projectPersonData.map((type)=> (
